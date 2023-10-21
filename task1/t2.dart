1b77957485c831d073865bad470acdafbef857fa
Doooaa <d98744330@gmail.com>
2023-10-22 02:45:07 +0300

    add t2

--- task1/t2.dart
+++ task1/t2.dart
@@ -1,12 +1,18 @@
  import 'dart:math';
 
 void main(List<String> args) {
-   List<int> l=[1,2,3,4,5];
+   List<int> l=[1,2,3,3,3,3,5];
    
    print(castom_any(l,((e) => e==3)));//found
    print(castom_any(l,((e) => e==8)));//not found
-}
 
+
+    print(".......................................................");
+    print(castom_some(l,((e) => e==3)));
+    print(".......................................................");
+    print(castom_every(l,((e) => e==3)));
+}
+//....................................................................
 bool castom_any(List<int> l,bool Function (int e)cback)
 {
 
@@ -14,8 +20,24 @@ bool castom_any(List<int> l,bool Function (int e)cback)
    return l.any(cback);
   
 }
+bool castom_every(List<int> l,bool Function (int e)cback)
+{
 
+            //take  bool Function (int)
+   return l.every(cback);
+  
+}
+//.......................................................................
 
+bool castom_some(List<int> l,bool Function (int e)cback)
+{
+   List frequancy=[];
+
+     frequancy= l.where(cback).toList();
+     if(frequancy.length>=2)
+      return true;
+     else return false;
+}
 
 
 
